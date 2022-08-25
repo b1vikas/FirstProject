5efa1407c77c557bb016f2227f9c4242f1aeafe2
Vikas <vikasbinjrawat@gmail.com>
2022-08-25 16:59:48 +0530

    FirstCommit

---
+++ Add.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+	// Write your code here
+	int n,c,i=1,sum=0,product=1;
+    cin>>n;
+    cin>>c;
+    if(n>=1 && n<=12)
+    {
+        if(c==1)
+    {
+       	while(i<=n)
+       	{
+       	    sum+=i;
+            i++;
+     	}
+        cout<<sum;
+    	
+    }
+    else if(c==2)
+    {
+        while(i<=n)
+       	{
+           	product*=i;
+            i++;
+       	}
+        cout<<product;
+    }
+    else
+        cout<<"-1";
+    }
+}
